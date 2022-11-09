a4df7cd1405d707ecbc4fbe29f6d588b8c12056b
7Cedars <Cedars7@proton.me>
2022-11-09 12:25:09 +0000

    updated comments in contract code

--- contracts/CertifyDoc.sol
+++ contracts/CertifyDoc.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-/*
-NB TODO: General description of contract! 
-NB: https://docs.ethers.io/v5/api/utils/strings/#Bytes32String 
-NB AND: https://docs.ethers.io/v5/api/utils/hashing/!! -- they have hashing algorithms. Of course! 
--- ethers has a js function to convert hash string to bytes32. THS is what I should use!  
+/**
+    * @dev This is a (very) simple solidity contract that relates a hash to a blockchain record
+    * consisting of two addresses, the date and time. 
+    * At the frontend, the hash is taken from an offline document 
 */
 
 contract CertifyDoc {
-    
-    // creates certificate struct. Consists of hash of document, issuer address, recipient address, a description and date of issuance. 
+    /**
+    * @dev creates certificate struct. Consists of hash of document, issuer address, recipient address, a description and date of issuance. 
+    * @param docHash The hash of a document. 
+    * @param issuer The address of the issuer.
+    * @param recipient The (optional) address of a recipient.
+    * @param datetime Time of issuing record. As given by block.timestamp. 
+    * @param description An (optional) description of the hashed document.  
+    */
     struct certificate {
         bytes32 docHash;
         address issuer;
@@ -18,25 +23,34 @@ contract CertifyDoc {
         uint datetime;
         string description;
     }
-    certificate[] public certificates; // array of certificates, expands as certificates are issued. 
+    /** @dev Array of certificates. */ 
+    certificate[] public certificates; // 
     
-    // event for logging creation of a certificate
+    /** @dev Event for logging creation of a certificate */ 
     event CertificateIssued(bytes32 indexed docHash, uint indexed index);
     
-    // to be able to retrieve certificates per document, issuer or recipient later on, we create three mappings: 
-    // a map of certificate indexes per document. (key = docHash, value is index of certificates)   
+    /**
+    * @dev to be able to retrieve certificates per document, issuer or recipient later on, we create four mappings: 
+    * @dev mapping1: certificate indexes *per document*. (key = docHash, value is index of certificates)   
+    */ 
     mapping(bytes32 => uint[]) public docHashMap; 
-
-    // a map of certificate indexes per issuer. (key = msg.sender, value is index of certificates) 
+    
+    /** @dev mapping2: certificate indexes *per issuer*. (key = msg.sender, value is index of certificates) */ 
     mapping(address => uint[]) public issuerMap;   
-
-    // a map of certificate indexes per document. (key = address, value is index of certificates)
+    
+    /** @dev mapping3: a map of certificate indexes per document. (key = address, value is index of certificates) */ 
     mapping(address => uint[]) public recipientMap;
 
-    // a map of index to address. (key = string of docHash, value address)
+    /** @dev mapping4: a map of index to address. (key = string of docHash, value address) */ 
     mapping(bytes32 => address) public docHashOwnerMap;
 
-    // creating certificates. Note that location of certificate in certificates array is also mapped in relation to docHash, issuer, and recipient.  
+    /**
+    * @dev creating certificates. 
+    * Note that location of certificate in certificates array is also mapped in relation to docHash, issuer, and recipient.  
+    * @param _docHash The hash of a document. 
+    * @param _recipient The (optional) address of a recipient.
+    * @param _description An (optional) description of the hashed document.  
+    */ 
     function certify(bytes32 _docHash, address _recipient, string memory _description) public {        
 
             certificate storage newCertificate = certificates.push(); 
@@ -50,9 +64,11 @@ contract CertifyDoc {
             issuerMap[msg.sender].push(certificates.length -1);
             recipientMap[_recipient].push(certificates.length -1);
 
-            // If this is the first time this docHash is being certified, the issuer is set as its owner. 
-            // The owner of the docHash can revoke any subsequent certificate linked to this docHash. 
-            // docHashOwner is transferrable (see function changeDocHashOwner below). 
+            /*
+            * If this is the first time this docHash is being certified, the issuer is set as its owner. 
+            * The owner of the docHash can revoke any subsequent certificate linked to this docHash. 
+            * docHashOwner is transferrable (see function changeDocHashOwner below). 
+            */
             if (docHashOwnerMap[_docHash] == 0x0000000000000000000000000000000000000000) {
                 docHashOwnerMap[_docHash] = msg.sender;
             }
@@ -61,36 +77,40 @@ contract CertifyDoc {
             emit CertificateIssued(_docHash, certificates.length -1); 
     }
 
-    // // Transfer ownership of docHash to another contract. -- This is for extended version. 
-    // function changeDocHashOwner(string memory _docHash, address newDocHashOwner) public {
-    //     require(msg.sender == docHashOwnerMap[_docHash], "You are not the owner of this docHash.");
-    //     docHashOwnerMap[_docHash] = newDocHashOwner;
-    // }
+    /**
+    * @dev Transfer ownership of docHash to another contract. -- This is for extended version. 
+    *
+    *    function changeDocHashOwner(string memory _docHash, address newDocHashOwner) public {
+    *        require(msg.sender == docHashOwnerMap[_docHash], "You are not the owner of this docHash.");
+    *        docHashOwnerMap[_docHash] = newDocHashOwner;
+    *    }
+    */
     
-    // retrieves index of certificates per docHash. 
+    /** @dev retrieves index of certificates per docHash.  */ 
     function checkDocHash(bytes32 _docHash) public view returns ( uint[] memory ) {  
         return (docHashMap[_docHash]);
     } 
-
-    // returns true if the  msg.sender is owner of the docHash.
+    
+    /** @dev returns true if the  msg.sender is owner of the docHash. */ 
     function checkOwner(bytes32 _docHash) public view returns ( bool ) {
         return (msg.sender == docHashOwnerMap[_docHash]); 
     }
-
-    // retrieves index of certificates per issuer of msg.sender.
+    
+    /** @dev retrieves index of certificates per issuer of msg.sender. */ 
     function checkIssuer(address _requestedAddress) public view returns ( uint[] memory ) {  
         return issuerMap[_requestedAddress];
     }
-
-    // retrieves index of certificates per recipient of msg.sender.
+    
+    /** @dev retrieves index of certificates per recipient of msg.sender. */ 
     function checkRecipient(address _requestedAddress) public view returns ( uint[] memory ) {
         return recipientMap[_requestedAddress];
     }
 
-    // Retrieves a single certificate based on index.
-    // Idea is that frontend first calls checkDocHash or checkSender, and subsequently does a loop to this function. 
-    // This places computational demand on side of frontend, not the blockchain backend. 
-    // Note that it IS possible to call certificates at random. Certificates are public! 
+    /** @dev Retrieves a single certificate based on index.
+    * Idea is that frontend first calls checkDocHash or checkSender, and subsequently does a loop to this function. 
+    * This places computational demand on side of frontend, not the blockchain backend. 
+    * Note that it IS possible to call certificates at random. Certificates are public! 
+    */
     function callCertificate(uint index) public view returns (
       bytes32, address, address, string memory, uint
       ) {
@@ -103,7 +123,10 @@ contract CertifyDoc {
         );
     }
 
-    // Issuer, recipient and docHashOwner have  ability to delete / revoke a document certification.
+    /** 
+    * @dev Issuer, recipient and docHashOwner have ability to delete certificate. 
+    * @param index: index of certificate to be revoked. 
+    */ 
     function revokeCertificate(uint index) public {
         require(msg.sender == certificates[index].issuer || 
             msg.sender == certificates[index].recipient ||
